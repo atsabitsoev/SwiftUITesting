513767eb813db6a0bb6065bd484fba886452e99a
417 <417@sirenabus.com>
2019-10-23 10:19:04 +0300

    Initial Commit

---
+++ SwiftUITesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITesting
+//
+//  Created by Ацамаз Бицоев on 23.10.2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
